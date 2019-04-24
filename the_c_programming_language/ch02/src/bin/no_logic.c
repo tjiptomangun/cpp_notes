7ac242eef40b9deb22a1cb3411fdee965ab88b58
hanky <hanky.acbb@telogic.com>
2019-04-24 21:53:44 +0700

    add no logic

---
+++ the_c_programming_language/ch02/src/bin/no_logic.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+#define MAX_LINE 1000
+
+int get_line(char s[], int lim){
+	int c, i;
+	i = 0;
+	while (i < lim - 1){
+		if ((c = getchar())== EOF)
+			break;
+		else if (c == '\n')
+			break;
+		else
+			s[i] = c;
+	}
+
+	if (c == '\n'){
+		s[i] = c;
+		++i;
+	}
+	s[i] = '\0';
+	return i;
+}
+
+void copy(char to [], char from[]){
+	int i;
+
+	i = 0;
+
+	while((to[i] = from[i])!= '\0')
+		++i;
+}
+
+int main (int argc, char **argv) {
+	int len;
+	int max;
+	char line[MAX_LINE];
+	char longest[MAX_LINE];
+
+	len = 0;
+	max = 0;
+
+	while((len = get_line(line, MAX_LINE)) > 0){
+		if (len > max){
+			max = len;
+			copy(longest, line);
+		}
+	}
+
+	if(max > 0)
+		printf("%s", longest);
+
+	return 0;
+}
