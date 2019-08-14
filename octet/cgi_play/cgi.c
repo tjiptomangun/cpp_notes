3da80219fea73ef7e64a765fe14891f09c6cf832
tjiptomangun <tjiptomangun@yahoo.com>
2019-08-14 15:54:16 +0700

    fix cgi

--- octet/cgi_play/cgi.c
+++ octet/cgi_play/cgi.c
@@ -8,6 +8,8 @@
 #include <stddef.h>
 #include "cgi.h"
 #include <assert.h>
+#include <stdbool.h>
+
 /**
  * NAME         : str_to_short
  * DESCRIPTION  : convert array of 4 char to short
@@ -21,8 +23,12 @@ int str_to_short(char *in, char *out) {
   char *inptr = in;
   char *outptr = out;
   int ret = 0;
-  if (inptr[0] >= 'a' && inptr[0] < 'f'){
+  if (inptr[0] >= 'a' && inptr[0] <= 'f'){
     ret |= (inptr[0] - 'a') + 10;
+    inptr ++;
+  }
+  else if (inptr[0] >= 'A' && inptr[0] <= 'F'){
+    ret |= (inptr[0] - 'A') + 10;
     inptr ++;
   }
   else {
@@ -30,8 +36,12 @@ int str_to_short(char *in, char *out) {
     inptr ++;
   }
   ret <<= 4;
-  if (inptr[0] >= 'a' && inptr[0] < 'f'){
+  if (inptr[0] >= 'a' && inptr[0] <= 'f'){
     ret |= (inptr[0] - 'a') + 10;
+    inptr ++;
+  }
+  else if (inptr[0] >= 'A' && inptr[0] <= 'F'){
+    ret |= (inptr[0] - 'A') + 10;
     inptr ++;
   }
   else {
@@ -39,8 +49,12 @@ int str_to_short(char *in, char *out) {
     inptr ++;
   }
   ret <<= 4;
-  if (inptr[0] >= 'a' && inptr[0] < 'f'){
+  if (inptr[0] >= 'a' && inptr[0] <= 'f'){
     ret |= (inptr[0] - 'a') + 10;
+    inptr ++;
+  }
+  else if (inptr[0] >= 'A' && inptr[0] <= 'F'){
+    ret |= (inptr[0] - 'A') + 10;
     inptr ++;
   }
   else {
@@ -48,8 +62,12 @@ int str_to_short(char *in, char *out) {
     inptr ++;
   }
   ret <<= 4;
-  if (inptr[0] >= 'a' && inptr[0] < 'f'){
+  if (inptr[0] >= 'a' && inptr[0] <= 'f'){
     ret |= (inptr[0] - 'a') + 10;
+    inptr ++;
+  }
+  else if (inptr[0] >= 'A' && inptr[0] <= 'F'){
+    ret |= (inptr[0] - 'A') + 10;
     inptr ++;
   }
   else {
@@ -83,18 +101,19 @@ int str_to_short(char *in, char *out) {
 }
 
 /**
- * NAME         : cgi
+ * NAME         : cgi_lai
  * DESCRIPTION  : parse cgi request bytes to ldap response
  * INPUT
  *      version : method version 1 or 2
  *      in      : message to parse
  *      out     : the result
+ *      is_cgi  : true if it is cgi, otherwise it is lai
  * RETURNS
  *      Success : number of bytes copied to out buffer
  *      Error   : CGI_E_INVALID_VERSION , version not supported
  *
  */
-int cgi(unsigned int version, char *in, char *out)  {
+int cgi_lai(unsigned int version, char *in, char *out, bool is_cgi)  {
   char *inptr = in;
   char *outptr = out;
   int ret = 0;
@@ -106,21 +125,21 @@ int cgi(unsigned int version, char *in, char *out)  {
     out[0] = in[1];
     outptr += 2;
     inptr += 2;
-    if (in[3] != 'f'){
+    if (in[3] != 'f' && in[3] != 'F'){
       outptr[0] = in[3];
       outptr ++;
     }
-    if (in[2] != 'f'){
+    if (in[2] != 'f' && in[2] != 'F'){
       outptr[0] = in[2];
       outptr ++;
-    }
+    }//mcc
     inptr+=2; 
     outptr[0] = '-';
     outptr ++;
 
     outptr[0] = inptr[1];
     outptr ++;
-    if (inptr[0] != 'f'){
+    if (inptr[0] != 'f' && in[3] != 'F'){
       outptr[0] = inptr[0];
       outptr ++;
     }
@@ -128,50 +147,64 @@ int cgi(unsigned int version, char *in, char *out)  {
     outptr[0] = '-';
     outptr ++;
   }
+  else {
+    inptr +=6;
+  }
   ret = str_to_short(inptr, outptr);
   inptr +=4;
   outptr += ret;
   outptr[0] = '-';
   outptr ++;
-  ret = str_to_short(inptr, outptr);
-  outptr += ret;
+  if (is_cgi) {
+    ret = str_to_short(inptr, outptr);
+    outptr += ret;
+  }
+  else {
+      outptr[0] = '0';
+      outptr ++;
+  }
   return outptr - out;
 }
-#ifdef UNIT_TEST
+#ifdef CGI_UNIT_TEST
 int main (int argc, char **argv) {
-  char test[] = {"25f0101ad2868d"};
+// char test[] = {"25f0101ad2868d"};
+// char test[] =   {"35f1121bd28681"};
+ char test[] = {"25F0231B124A8D"};
+// char test[] = {"25f2231f121aff"};
+// char test[] = {"25F03204C622FA"};
+//  char test[] = {"25f0931f1f24f0"};
   char out[18]; 
   memset (out, 0, 18);
-  int ret = cgi(2, test, out);
+  int ret = cgi_lai(2, test, out, 14);
   printf("%s\n", out);
   assert(ret > 0);
 
   memset (out, 0, 18);
-  ret = cgi(1, &test[6], out);
+  ret = cgi_lai(1, test, out, 14);
   printf("%s\n", out);
   assert(ret > 0);
   memset (out, 0, 18);
-  ret = cgi(0, &test[6], out);
+  ret = cgi_lai(0, &test[6], out, 14);
   assert(ret < 0);
 }
 #endif
 
-#ifdef PERF_TEST
+#ifdef CGI_PERF_TEST
 #include <time.h>
 #include <unistd.h>
 #define BILLION 1000000000.0
 
 int main (int argc, char **argv) {
-  char test[] = {"25f0101ad2868d"};
+ // char test[] = {"25f0101ad2868d"};
+  char test[] =   {"35f1101ad2868d"};
   char out[18]; 
   struct timespec start, end;
   memset (out, 0, 18); 
   clock_gettime(CLOCK_REALTIME, &start); 
-  cgi(2, test, out);
+  cgi_lai(2, test, out, 14);
   clock_gettime(CLOCK_REALTIME, &end); 
   double time_spent = (end.tv_nsec - start.tv_nsec); 
   printf ("Time elapsed is %f secs\n", time_spent/BILLION); 
-  printf ("Time elapsed is %f nsecs\n", time_spent); 
 }
 #endif
 
